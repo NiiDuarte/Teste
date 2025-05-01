d9ef31eb8b1f88cc2726ee9d3bde617dbf38eed5
Monique Duarte <moniqueduarte5623@gmail.com>
2025-05-01 20:56:52 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById('');
-const formulario = document.getElementById('form-container');
-
-botao.addEventListener('click', () => {
-    formulario.classList.toggle('escondido');
-});
