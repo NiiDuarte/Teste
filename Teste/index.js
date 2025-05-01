01567da774af5c9b36a06acb098261dd1d5dd135
Ruan almeida <ruanalmeida.contato>
2025-05-01 17:52:12 -0300

    Adiciona Teste como pasta comum

---
+++ Teste/index.js
@@ -0,0 +1,9 @@
+function btn() {
+    const botao = document.getElementById('botao');
+    botao.style.display = 'flex';
+}
+
+function fecharPopup() {
+    const botao = document.getElementById('botao');
+    botao.style.display = 'none';
+}
