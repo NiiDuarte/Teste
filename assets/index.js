6faca6a893da7508fe9d1b8f39409b7c9325e5da
Ruan almeida <ruanalmeida.contato>
2025-05-01 21:24:33 -0300

    forms atualizado

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,18 @@
+function envia(event) {
+    event.preventDefault();
+    alert('Formulário enviado com sucesso!')
+    fecharPopup();
+    
+}
+
 function btn() {
     const botao = document.getElementById('botao');
     botao.style.display = 'flex';
+
 }
 
 function fecharPopup() {
     const botao = document.getElementById('botao');
     botao.style.display = 'none';
 }
+
