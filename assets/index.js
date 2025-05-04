7ec45fe2997a98ba5193fe6909fb61d8bd61aa97
Ruan almeida <ruanalmeida.contato>
2025-05-04 12:08:26 -0300

    adicionei um carousel no projeto, mas com imagens ficticias

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,25 @@
+
+/*esses scripts abaixo são referentes ao carousel*/
+const slides = document.querySelector('.slides');
+const slidesCount = document.querySelectorAll('.slide').length;
+let index = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+    index = (index + 1) % slidesCount;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+})
+
+document.querySelector('.prev').addEventListener('click', () => {
+    index = (index - 1 + slidesCount) % slidesCount;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+})
+
+setInterval(() => {
+    index = (index + 1) % slidesCount;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}, 5000);
+
+/* esses scripts abaixo são referentes ao processo de compra do serviço*/
 function envia(event) {
     event.preventDefault();
     alert('Formulário enviado com sucesso!')
